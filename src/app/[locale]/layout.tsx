28b57ffd133278994a691f290a661a7ee66610da
Amna <amna26103@gmail.com>
2025-10-05 20:17:01 +0000

    Try fixing this error: `Runtime Error: Error: Missing <html> and <body>

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -1,17 +1,5 @@
-import type { Metadata } from 'next';
-import '../globals.css';
-import { Toaster } from '@/components/ui/toaster';
-import { cn } from '@/lib/utils';
-import { FirebaseClientProvider } from '@/firebase';
 import {NextIntlClientProvider} from 'next-intl';
 import {getMessages} from 'next-intl/server';
-
-export const metadata: Metadata = {
-  title: "Femmora: Women's Wellness Hub",
-  description:
-    'A safe and empowering space for women to connect, grow, and thrive.',
-  viewport: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
-};
 
 export default async function LocaleLayout({
   children,
@@ -23,27 +11,8 @@ export default async function LocaleLayout({
   const messages = await getMessages();
 
   return (
-    <html lang={locale} suppressHydrationWarning>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="anonymous"
-        />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Alegreya:ital,wght@0,400..900;1,400..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
-      <body className={cn('font-body antialiased h-screen flex flex-col')}>
-        <NextIntlClientProvider locale={locale} messages={messages}>
-          <FirebaseClientProvider>
-            {children}
-            <Toaster />
-          </FirebaseClientProvider>
-        </NextIntlClientProvider>
-      </body>
-    </html>
+    <NextIntlClientProvider locale={locale} messages={messages}>
+      {children}
+    </NextIntlClientProvider>
   );
 }
