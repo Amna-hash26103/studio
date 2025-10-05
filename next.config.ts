043746fd5fb957e91bdf15b37068acf8a8cdfd69
Amna <amna26103@gmail.com>
2025-10-05 14:35:38 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
 
 const nextConfig: NextConfig = {
   /* config options here */
+  turbopack: {},
   typescript: {
     ignoreBuildErrors: true,
   },
