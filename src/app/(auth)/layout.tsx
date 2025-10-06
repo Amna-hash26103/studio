f01ac2bd9a8ff2623d7d68467721cf68dd0641c1
Amna <amna26103@gmail.com>
2025-10-06 06:54:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,10 +1,9 @@
 'use client';
-import { FirebaseClientProvider } from '@/firebase';
 
 export default function AuthLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <FirebaseClientProvider>{children}</FirebaseClientProvider>;
+  return <>{children}</>;
 }
