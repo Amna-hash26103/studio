dad063853b649e0aad16d949971a47695de27fe3
Amna <amna26103@gmail.com>
2025-10-07 17:57:18 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.js
+++ next.config.js
@@ -1,10 +1,22 @@
+
 /** @type {import('next').NextConfig} */
 const createNextIntlPlugin = require('next-intl/plugin');
  
 const withNextIntl = createNextIntlPlugin('./src/i18n.ts');
  
 const nextConfig = {
-  // Your existing Next.js config ...
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'i.postimg.cc',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+    ],
+  },
 };
  
 module.exports = withNextIntl(nextConfig);
